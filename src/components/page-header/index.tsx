0b7067e43c0315be5589330b429ec4d92474a22b
Diego Goulart <diego.goulart@edifyeducation.com.br>
2023-10-17 23:39:00 -0300

    first commit

---
+++ src/components/page-header/index.tsx
@@ -0,0 +1,17 @@
+import { MainNav } from '@/components/main-nav'
+import { ModeToggle } from '@/components/mode-toggle'
+
+export function SiteHeader() {
+  return (
+    <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+      <div className="container flex h-14 items-center">
+        <MainNav />
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <nav className="flex items-center">
+            <ModeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: 'Ai Dashboard',
+  url: 'http://localhost:3000',
+  ogImage: 'https://ui.shadcn.com/og.jpg',
+  description: 'My AI Dashboard',
+}
+
+export type SiteConfig = typeof siteConfig
